f394b71244d8a61bfd1595cc7db50282ef26fb02
Joshua123right <106165427+Joshua123right@users.noreply.github.com>
2022-11-11 21:07:59 -0500

    Routing-Finish
    Done routing enabled CSS Tailwind

---
+++ pages/directmessage.js
@@ -0,0 +1,7 @@
+export default function directmessage() {
+    return (
+        <>
+        <h1>directmessage</h1>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-
+import Link from 'next/link'
 export default function Home() {
   return (
-    <>
-  <div className="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-    <div className="card-body">
-      <h2 className="card-title">Shoes!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div className="card-actions justify-end">
-        <button className="btn btn-primary">Buy Now</button>
-      </div>
-    </div>
-  </div>
-  </>
+    <h1>Main Page</h1>
   )
 }
