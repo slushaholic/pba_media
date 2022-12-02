d7105dbd844e6fd8f08d106a54aff572e810a7ca
Joshua123right <106165427+Joshua123right@users.noreply.github.com>
2022-12-02 12:41:21 -0500

    HomePage
    Finished Styling for homepage

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,26 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Card from '../components/Card'
 import SendPost from '../components/SendPost'
+import RecivedPost from '../components/RecivedPost'
 export default function Home() {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='w-3/4 ml-28'>
-    <Card>
-        <SendPost />
-    </Card>
+    <div className='container grow'>
+       <div className='flex justify-center items-center'>
+        <div className='w-3/4 ml-64'>
+          <Card>
+            <SendPost />
+          </Card>
+        </div>
+    </div>
+    <div className='flex justify-center items-center my-5'>
+      <div className='w-3/4 ml-64'>
+        <Card>
+          <RecivedPost />
+        </Card>
       </div>
     </div>
+  </div>
+   
       
    
   )
