d767d90c182c5e6be353ae0b21bfca4ad303a044
Joshua123right <106165427+Joshua123right@users.noreply.github.com>
2022-12-02 13:32:20 -0500

    ProfilePage
    Started wanted to push for home PC

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,9 @@
 import Card from "../components/Card"
+import ProfileHeader from "../components/ProfileHeader"
 export default function Profile() {
     return (
-        <div className="flex justify-center items-center border border-outline">
-            <h1>This test</h1>
+        <div className="w-3/4 flex justify-center items-center h-5 ">
+            <ProfileHeader />
         </div>
     )
 }
