c6568dab2984b9a242efd88faf30471be5d4a62c
slushaholic <tomsibe@gmail.com>
2022-12-08 11:10:37 -0500

    summary

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,28 @@
 import NextAuth from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
+        
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
     ],
+    callbacks: {
+        async jwt({ token, account}) {
+            if (account) {
+                token.accessToken = account.access_token 
+            }
+            return token
+        },
+        async session ({ session, token, user}) {
+            session.accessToken = token.accessToken
+            return session
+        }
+    },
+    secret: "toast",
+    debug: true
 }
 
 export default NextAuth(authOptions)
