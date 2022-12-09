fc165a5d74d771fa075869bf4499d3442e98a052
slushaholic <tomsibe@gmail.com>
2022-12-09 11:09:45 -0500

    summary

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,24 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import clientPromise from '../../../lib/mongodb'
 
 export const authOptions = {
     providers: [
         
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
         })
     ],
+    adapter: MongoDBAdapter(clientPromise),
     callbacks: {
         async jwt({ token, account}) {
             if (account) {
@@ -19,7 +29,8 @@ export const authOptions = {
         async session ({ session, token, user}) {
             session.accessToken = token.accessToken
             return session
-        }
+        },
+        
     },
     secret: "toast",
     debug: true
