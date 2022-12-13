6231d7c3ab3ac16c25f17df5aa0094bf059df18e
Weldyyy <106244796+Weldyyy@users.noreply.github.com>
2022-12-13 12:01:12 -0500

    Terms

--- components/Policy.js
+++ components/Policy.js
@@ -1,9 +1,9 @@
 export default function Policy() {
     return (
         <>
-        <h1 className="font-semibold text-[20px]">Policy</h1>
+        <h1 className="font-semibold text-[20px]">Terms of Use</h1>
         <div className="">
-            <p>Ethan put it here</p>
+            <p>The Terms of Use by PBA Media is an agreement between us and you that insures that you will use this website respectfully. You have agreed to our Terms of Use when you made an account for PBA Media.  AKA Kyle</p>
         </div>
         </>
         
--- components/sidebar.js
+++ components/sidebar.js
@@ -26,7 +26,7 @@ export default function SideBar() {
         </div>
 
         <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
           </svg>
           <ul>
