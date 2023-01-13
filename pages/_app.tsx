a6c4998db78b4447070a5f079eb383649e1a9e2f
slushaholic <tomsibe@gmail.com>
2023-01-12 22:21:10 -0500

    Vercel Fix #2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Link from 'next/link'
 import '../styles/navbar.css'
-import SideBar from '../components/SideBar'
+import SideBar from "../components/sidebar"
 import Footer from '../components/Footer'
 import {SessionProvider} from 'next-auth/react'
 
