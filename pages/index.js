69168e2caecad098d406098d70bc54146c26df87
slushaholic <tomsibe@gmail.com>
2023-04-18 12:07:44 -0400

    Filling out posts 4/18

--- pages/index.js
+++ pages/index.js
@@ -25,13 +25,13 @@ export default function Home() {
                 <div className="inline-flex">
                     <div className="pic">
                     {/* Profile PICTRUE GOES IN THE SRC */}
-                        <img src="https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXQlMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
                             className="h-12 rounded-full"
                         />
                     </div>
                     <div className="Name">
                     {/* The name of the profile goes  in the A tag don't change the shared a post thing */}
-                        <p className="p-4"><a className="font-semibold">Josh Wise</a> Shared a post</p>
+                        <p className="p-4"><a className="font-semibold">Elon Musk</a> Shared a post</p>
                     </div>
                 </div>
                 <div className="float-right inline-flex my-2">
@@ -45,13 +45,19 @@ export default function Home() {
                 <div className="text container">
                     <div className="text-sm p-1">
                     {/* Proffesional paragraph of message sent */}
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus tortor, posuere vitae erat vel, placerat volutpat turpis. Nulla sit amet dolor eget arcu dignissim auctor vel non nulla. Nunc mattis orci non dui scelerisque, eu accumsan nisl ullamcorper. Sed faucibus sem quam, at auctor lorem fermentum fringilla. Donec et leo porta, consectetur massa in, dapibus mauris. Vestibulum vitae imperdiet odio. Proin maximus dui quis odio vehicula, vitae auctor augue elementum. Orci varius natoque penatibus et magnis dis parturient montes.
+                        Had a great time at the conference today! Loving the California sun!
                     </div>
                     <div className="rounded-md overflow-hidden w-full">
                     {/* An image if the proffessional sender sent a proffessional image */}
-                        <img src="https://images.unsplash.com/photo-1612827788868-c8632040ab64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfD_J1bYZ0EMFdPuvKf46QUWnMerHNr2_BDnr2S0H2wg&usqp=CAU&ec=48600112https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.klook.com%2Fcity%2F124-los-angeles-things-to-do%2F&psig=AOvVaw3V7OPJU0hFoysIvWAT_i-J&ust=1681919215140000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKjh7ePjs_4CFQAAAAAdAAAAABAIttps://images.unsplash.com/photo-1612827https://res.klook.com/image/upload/fl_lossy.progressive,w_800,c_fill,q_85/Mobile/City/uni4vfxknblsldmpxmxq.jpg788868-c8632040ab64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                             className="object-siz-down max-w-full max-h-72"
                         />
+                    </div>
+                    <div className='inline-flex my-2 p-2'>
+                      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj0LKQb4WDl6GiyEj00Hm0PctCtOUs9fvN3BfoJhyyPg&usqp=CAU&ec=48600112'
+                      className="h-12 rounded-full"
+                      />
+                      <p className="p-4"><a className="font-semibold">Donald Trump</a> Can't wait to hangout later Elon.</p>
                     </div>
                 </div>
             </div>
@@ -481,9 +487,7 @@ export default function Home() {
         {/* End Copy here! */}
         <div className='flex justify-center items-center my-5'>
           <div className='my-2 w-10/12'>
-            <Card>
-              <UserPost />
-            </Card>
+            
           </div>
         </div>
         {/* End Copy here! */}
