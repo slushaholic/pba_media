4da5cf10a84b4ac3afd8e2451837107512e1422a
slushaholic <tomsibe@gmail.com>
2023-04-24 11:48:51 -0400

    summary

--- pages/index.js
+++ pages/index.js
@@ -73,7 +73,7 @@ export default function Home() {
                                 <div className="inline-flex">
                                     <div className="pic">
                                         {/* Profile PICTRUE GOES IN THE SRC */}
-                                        <img src="https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9ydHJhaXQlMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCicgGzuJ5PvFUjJ5_2nkyPa5bgLeOKmOVfOWNN2ipIA&usqp=CAU&ec=48600112"
                                             className="h-12 rounded-full"
                                         />
                                     </div>
@@ -143,7 +143,7 @@ export default function Home() {
                                     </div>
                                     <div className="Name">
                                         {/* The name of the profile goes  in the A tag don't change the shared a post thing */}
-                                        <p className="p-4"><a className="font-semibold">Josh Wise</a> Shared a post</p>
+                                        <p className="p-4"><a className="font-semibold">Bruce Wayne</a> Shared a post</p>
                                     </div>
                                 </div>
                                 <div className="float-right inline-flex my-2">
@@ -157,7 +157,7 @@ export default function Home() {
                                 <div className="text container">
                                     <div className="text-sm p-1">
                                         {/* Proffesional paragraph of message sent */}
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus tortor, posuere vitae erat vel, placerat volutpat turpis. Nulla sit amet dolor eget arcu dignissim auctor vel non nulla. Nunc mattis orci non dui scelerisque, eu accumsan nisl ullamcorper. Sed faucibus sem quam, at auctor lorem fermentum fringilla. Donec et leo porta, consectetur massa in, dapibus mauris. Vestibulum vitae imperdiet odio. Proin maximus dui quis odio vehicula, vitae auctor augue elementum. Orci varius natoque penatibus et magnis dis parturient montes.
+                                        Can't wait to see all the competitors at Anaheim this year!
                                     </div>
                                     <div className="rounded-md overflow-hidden w-full">
                                         {/* An image if the proffessional sender sent a proffessional image */}
